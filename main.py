d987f6a195bd4ec94a344f3124447b0417a2565a
ashenn_0 <ashenn_0@outlook.com>
2024-07-16 16:23:42 +0800

    init

---
+++ main.py
@@ -0,0 +1,79 @@
+import unicodedata
+import sys
+import json
+import hashlib
+import requests
+
+app_id = '' # needed
+key = '' # needed
+salt = '' # needed
+l_from = ''
+l_to = ''
+domain = '' # needed
+
+def is_english(ch):
+    return ch.isalpha() and unicodedata.name(ch).startswith('LATIN')
+
+def is_chinese(ch):
+    if ' radicals' in unicodedata.name(ch) or \
+        unicodedata.east_asian_width(ch) in ['F', 'W']:
+        return True
+    return False
+
+def tec_laungrage(q):
+    for c in q:
+        if is_chinese(c):
+            return True
+        elif is_english:
+            return False
+    return True
+
+def getQ():
+    ans = ''
+    global l_from,l_to
+    if len(sys.argv) < 2:
+        print('wrong input')
+        return 
+    for arg in sys.argv:
+        if arg == sys.argv[0]:
+            continue
+        ans += arg
+    if tec_laungrage(ans):
+        l_from = 'zh'
+        l_to = 'en'
+    else:
+        l_from = 'en'
+        l_to = 'zh'
+    return ans
+
+def s2md5(sign):
+    md5_obj = hashlib.md5()
+    md5_obj.update(sign.encode())
+    return md5_obj.hexdigest()
+
+
+def httpC(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text
+        content = json.loads(content)
+        dst = content["trans_result"][0]["dst"]
+        return dst
+
+def tran(q):
+    sign = app_id + q + salt + domain + key
+    sign = s2md5(sign)
+    url = 'http://api.fanyi.baidu.com/api/trans/vip/fieldtranslate' +   \
+    '?q=' + q +                                                         \
+    '&from=' + l_from +                                                 \
+    '&to=' + l_to +                                                     \
+    '&appid=' + app_id +                                                \
+    '&salt=' + salt +                                                   \
+    '&domain=' + domain +                                               \
+    '&sign=' + sign
+    res = httpC(url)
+    print(res)
+
+q = getQ();
+if q != None:
+    tran(q)
